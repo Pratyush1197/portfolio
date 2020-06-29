b87f0d03d7059f6eb38660b5ecb300a3267b86c0
Pratyush1197 <pratyushnarain97@gmail.com>
2020-06-29 23:05:15 +0530

    Updates

---
+++ precache-manifest.a6968b4d211da7ec43962c5ef8de3f44.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182e550d33a26b3b1ff910de09668056",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "54e0046d1888ae9bfbf8",
+    "url": "/portfolio/static/css/2.1cbcebc2.chunk.css"
+  },
+  {
+    "revision": "ec699c6f03c4deb864f1",
+    "url": "/portfolio/static/css/main.083c69ec.chunk.css"
+  },
+  {
+    "revision": "54e0046d1888ae9bfbf8",
+    "url": "/portfolio/static/js/2.09da1e95.chunk.js"
+  },
+  {
+    "revision": "ec699c6f03c4deb864f1",
+    "url": "/portfolio/static/js/main.42ed4c55.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "c6f4bd6319e78d203742168635f923aa",
+    "url": "/portfolio/static/media/Pratyush_Narain_resume.c6f4bd63.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "717d83da5dd258a6fd95bf9eb0f753c0",
+    "url": "/portfolio/static/media/js.717d83da.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
